ef05a39e424a92b710bcda408c21edefdfbe3370
Vetchadines <vsvdineshkumar118@gmail.com>
2021-11-02 15:42:51 +0000

    git pull origin main

--- method.js
+++ method.js
@@ -1,3 +1,12 @@
+//web3 ethereum talker  dependency
+const Web3 = require('Web3')
+
+//transaction crafting dependency
+const Tx = require('ethereums-tx')
+
+require('dotenv').config()
+
+
 // connect to our contract
 
 //create a transaction to execute a method on the contract
